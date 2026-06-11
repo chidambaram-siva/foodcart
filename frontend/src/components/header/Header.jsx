b5867bf71915b1edb62fe4de942e4a63a423d2a9
chidambaram-siva <smartsiva2480@gmail.com>
2026-06-12 00:48:46 +0530

    Add background image to header component
    Updated header component to include a background image.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./Header.css"
+import header_img from "../../assets/header_img.png";
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ backgroundImage: `url(${header_img})` no-repeat}}>
         <div className='header-content'>
             <h2>Order your favourite food here</h2>
             <p>Explore and order a variety of delicious meals, snacks, and beverages from local vendors. Whether you're craving spicy, sweet, or savory, we’ve got something for every taste. Fast delivery, easy ordering—your favorite food is just a click away!</p>
