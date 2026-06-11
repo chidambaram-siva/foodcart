4edb38f639d23322452ed1702cee0609b2f8fa75
chidambaram-siva <smartsiva2480@gmail.com>
2026-06-11 22:52:03 +0530

    db commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectdb = async () => {
   try {
+    console.log("Connecting...");
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Database connected");
+    console.log("Database Connected");
   } catch (error) {
-    console.error("DB connection error:", error);
+    console.error("Full Error:");
+    console.error(error);
   }
 };
