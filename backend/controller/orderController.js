5338f4b2082244c97184f7b87c0c36b8bb7d0d6e
chidambaram-siva <smartsiva2480@gmail.com>
2026-06-12 14:16:38 +0530

    url change

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "";
+  const frontend_url = "http://localhost:5173/";
 
   try {
     const newOrder = new orderModel({
